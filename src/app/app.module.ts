5ff7f8df79aacffa6c9fc1c60baac02c834b2a4f
Liniya Merin Mathew <Liniya@NTP-LAP-788.chn.nestgroup.net>
2022-11-24 12:25:46 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,18 @@ import { AppComponent } from './app.component';
 import { PatientEntryComponent } from './patient-entry/patient-entry.component';
 import { PatientSearchComponent } from './patient-search/patient-search.component';
 import { PatientDeleteComponent } from './patient-delete/patient-delete.component';
+import { RouterModule } from '@angular/router';
+
+const myRoute:Routes=[
+  {
+    path:"",
+    component:PatientEntryComponent
+  }
+  {
+    path:"search",
+    component:P
+  }
+]
 
 @NgModule({
   declarations: [
@@ -16,7 +28,8 @@ import { PatientDeleteComponent } from './patient-delete/patient-delete.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
